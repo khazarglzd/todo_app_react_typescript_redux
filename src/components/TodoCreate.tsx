12dea1b48280dc1974fbf313cef9d6cfe94af459
khazarglzd <khazarguluzade@gmail.com>
2024-12-29 19:17:55 +0400

    adding createTodo reducer

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { TodoType } from "../types/Types";
+import { createTodo } from "../redux/todo-slice";
 
 export default function TodoCreate() {
+  const dispatch = useDispatch();
+
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleCreateTodo = () => {
+    if (newTodo.trim().length == 0) {
+      alert("Add some Todo");
+      return;
+    }
+    const payload: TodoType = {
+      id: Math.floor(Math.random() * 999999),
+      content: newTodo,
+    };
+
+    dispatch(createTodo(payload));
+    setNewTodo("");
+    console.log("added");
+  };
+
   return (
     <div className="todo-create">
-      <input type="text" className="todo-input" placeholder="Add todo..." />
-      <button className="todo-create-button"> Create</button>
+      <input
+        type="text"
+        className="todo-input"
+        value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setNewTodo(e.target.value)
+        }
+        placeholder="Add todo..."
+      />
+      <button onClick={handleCreateTodo} className="todo-create-button">
+        {" "}
+        Create
+      </button>
     </div>
   );
 }
--- src/redux/todo-slice.tsx
+++ src/redux/todo-slice.tsx
@@ -1,14 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { TodoInitialState, TodoType } from "../types/Types";
 
-const initialState = {};
+const initialState: TodoInitialState = {
+  todos: [],
+};
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
-  reducers: {},
+  reducers: {
+    createTodo: (state: TodoInitialState, action: PayloadAction<TodoType>) => {
+      state.todos = [...state.todos, action.payload];
+    },
+  },
 });
 
-export const {} = todoSlice.actions;
+export const { createTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
